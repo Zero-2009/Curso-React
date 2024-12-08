fc2a577efdee669e6f53ea12c9a3d048d741517d
zeritx <luisfabriziosarachaga@gmail.com>
2024-12-08 20:13:31 +0100

    Realize the operations of numbers

--- Proyecto-Personal/src/components/InterfaceCreate.jsx
+++ Proyecto-Personal/src/components/InterfaceCreate.jsx
@@ -1,31 +1,64 @@
 import "./interface.css"
 
+//Tareas:
+//1) Que no se pueda tener mas de un operador
+//2) Sumar mas de dos numeros 
 function InterfaceCreate() {
     // Ver tema de hoooks en REACTs
-    const numberToOperation = () => {
-
-    }
+    let valueOne = "";
+    let valueTwo = "";
+    let operation = "";
+    let result = "";
     const addNumber = (e) => {
         let value = e.target.innerText;
         let numberView = document.getElementById("view-data");
         let numberPass = numberView.textContent;
         numberView.innerText = numberPass + value;
+        if (operation === "") {
+            valueOne = valueOne + value;
+        } else {
+            valueTwo = valueTwo + value;
+        }
     }
     const addOperator = (e) => {
         let numberView = document.getElementById("view-data");
         let operator = e.target.innerText;
         let operatorPass = numberView.textContent;
         numberView.innerText = operatorPass + operator;
+        operation = operator;
+    }
+    const showResult = () => {
+        let numbers_operated = document.getElementById("view-NmsOperated")
+        let numberComplete = document.getElementById("view-data");
+        let operatorShow = numberComplete.textContent;
+        numbers_operated.style.display = "block";
+        if (operation == "+") {
+            result = Number(valueOne) + Number(valueTwo);
+            numberComplete.innerText = result;
+        } if (operation == "-") {
+            result = Number(valueOne) - Number(valueTwo);
+            numberComplete.innerText = result;
+        } if (operation == "*") {
+            result = Number(valueOne) * Number(valueTwo);
+            numberComplete.innerText = result;
+        } if (operation == "/") {
+            result = Number(valueOne) / Number(valueTwo);
+            numberComplete.innerText = result;
+        }
     }
     const clearNumber = () => {
         let numberView = document.getElementById("view-data");
         numberView.innerText = "";
+        valueOne = "";
+        valueTwo = "";
+        operation = "";
     }
     return (
         <>
             <section className="interface-calculator">
                 <section className="interface-view">
-                    <div id="view-data"></div>
+                    <div className="view-numbers-operated" id="view-NmsOperated"></div>
+                    <div className="view-numbers" id="view-data"></div>
                 </section>
                 <div className="interface-container">
                     <section className="interface-operations-numbers">
@@ -39,7 +72,7 @@ function InterfaceCreate() {
                             <button onClick={addNumber}>7</button>
                             <button onClick={addNumber}>8</button>
                             <button onClick={addNumber}>9</button>
-                            <button>=</button>
+                            <button onClick={showResult}>=</button>
                             <button onClick={addNumber}>0</button>
                             <button onClick={addNumber}>.</button>
                         </div>
