ad35a81f6c0f22d7c0d973bc4f0c24267440f7f8
Zero-2009 <luisfabriziosarachaga@gmail.com>
2024-12-19 23:52:10 +0100

    The estructure is near to finished

--- Interface-Usuario/src/components/Interface.jsx
+++ Interface-Usuario/src/components/Interface.jsx
@@ -3,9 +3,19 @@ import "./InterFace.css"
 function InterFaceCreate() {
     return (
         <>
-            <section>
-                <section></section>
-                <section></section>
+            <section className="interface-global">
+                <div><img src="" alt="" /></div>
+                <div className="User-Password">
+                    <input type="text" />
+                    <input type="password" />
+                </div>
+                <div className="">
+                    <button type="button">Enter</button>
+                </div>
+                <div className="">
+                    <div>enter as a guest </div>
+                    <div>recover password</div>
+                </div>
             </section>
         </>
     )
